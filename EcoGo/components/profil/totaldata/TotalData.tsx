994e5f0518abbd2c925354bbbc4b76b26a47fb19
Thomas Planchard <91249646+thomas-planchard@users.noreply.github.com>
2024-08-20 17:38:14 +0200

    Movement Detection

--- EcoGo/components/profil/totaldata/TotalData.tsx
+++ EcoGo/components/profil/totaldata/TotalData.tsx
@@ -2,25 +2,60 @@ import {
   View,
   Text
 } from "react-native";
+import { useAuth } from "@/context/AuthContext";
+import { useState, useEffect } from "react";
+import { doc, onSnapshot} from  'firebase/firestore';
+import {db} from '../../../FirebaseConfig';
 import styles from "./totaldata.style"
 
 
 
 export default function TotalData () {
 
+  const {user} = useAuth();
+  const [carbonFootprint, setCarbonFootprint] = useState(0);
+
+
+  useEffect(() => {
+    let unsubscribe;
+    const fetchCarbonFootprint = async () => {
+      try {
+        if (user && user.userId) {
+          const userDataRef = doc(db, "userData", user.userId);
+          unsubscribe = onSnapshot(userDataRef, (doc) => {
+            if (doc.exists()) {
+              const userData = doc.data();
+              setCarbonFootprint(userData.carbonFootprint || 0);
+            }
+          });
+        }
+      } catch (error) {
+        console.error("Error fetching carbon footprint:", error);
+      }
+    };
+
+    fetchCarbonFootprint();
+
+    return () => {
+      if (unsubscribe) {
+        unsubscribe();
+      }
+    };
+  }, [user]);
+
   return (
     <View style={styles.mainContainer}>
     <View style={styles.container}>
       <View style= {styles.column}>
-        <Text style={styles.userInformationMain}>2.33M</Text>
+        <Text style={styles.userInformationMain}>0</Text>
         <Text style={styles.userInformationSecondary}>Steps</Text>
       </View>
       <View style= {styles.column}>
-        <Text style={styles.userInformationMain}>222LBS</Text>
+        <Text style={styles.userInformationMain}>{carbonFootprint} kg</Text>
         <Text style={styles.userInformationSecondary}>Carbon Footprint</Text>
       </View>
       <View style= {styles.column}>
-        <Text style={styles.userInformationMain}>1.9K</Text>
+        <Text style={styles.userInformationMain}>0</Text>
         <Text style={styles.userInformationSecondary}>Coins</Text>
       </View>
     </View>
